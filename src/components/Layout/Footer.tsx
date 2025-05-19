213337b9bdfc6a95acc970be6a52a5e426e6d00c
alexander <san4ezy@gmail.com>
2025-05-19 18:14:15 +0300

    fix footer style

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -2,10 +2,10 @@
 
 function Footer() {
     return (
-        <footer className="bg-neutral text-neutral-content p-4 text-center">
+        <footer className="bg-neutral text-neutral-content p-4">
             <div className="container mx-auto">
-                <p>The place where education goes in a breath.</p>
-                <p>&copy; 2025 eDoo App</p>
+                <p className=" text-center">The place where education goes in a breath.</p>
+                <p className=" text-center">&copy; 2025 eDoo App</p>
             </div>
         </footer>
     )
